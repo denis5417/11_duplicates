66026127c68ea58dec65db0ff968f23984f621ac
denis5417 <denisz.pol@yandex.ru>
2017-07-24 08:00:07 +0300

    Update

--- duplicates.py
+++ duplicates.py
@@ -8,7 +8,6 @@ def get_all_files(dir):
     path_f = []
     for dirs, subdirs, files in os.walk(dir):
         for f in files:
-            if not f.startswith("."):
                 path = os.path.join(dirs, f)
                 path_f.append(path)
     return sorted(path_f, key=lambda file: os.path.basename(file))
@@ -18,14 +17,22 @@ def are_files_duplicates(file1, file2):
     return os.path.basename(file1) == os.path.basename(file2) and os.path.getsize(file1) == os.path.getsize(file2)
 
 
+def is_dir_exist(dir):
+    return os.path.exists(dir)
+
+
 def find_duplicates(dir):
     duplicates = []
-    if not os.path.exists(dir):
-        return DIR_NOT_EXIST
     path_f = get_all_files(dir)
-    for counter in range(0, len(path_f)-1):
-        if are_files_duplicates(path_f[counter], path_f[counter + 1]):
-                duplicates.append((path_f[counter], path_f[counter + 1]))
+    files_iterator = iter(path_f)
+    next(files_iterator)
+    try:
+        for file in path_f:
+            next_file = next(files_iterator)
+            if are_files_duplicates(file, next_file):
+                duplicates.append((file, next_file))
+    except StopIteration:
+        pass
     return duplicates
 
 
@@ -36,10 +43,11 @@ def arg_parser():
 
 
 if __name__ == '__main__':
-    duplicates = find_duplicates(arg_parser().dirpath)
-    if duplicates == -1:
-        print("Такой директории не существует")
-    elif duplicates:
-        print("\n".join("{} дублируется с {}".format(duplicate[0], duplicate[1]) for duplicate in duplicates))
+    if is_dir_exist(arg_parser().dirpath):
+        duplicates = find_duplicates(arg_parser().dirpath)
+        if duplicates:
+            print("\n".join("{} дублируется с {}".format(duplicate[0], duplicate[1]) for duplicate in duplicates))
+        else:
+            print("Дубликатов нет")
     else:
-        print("Дубликатов нет")
+        print("Такой директории не существует")
